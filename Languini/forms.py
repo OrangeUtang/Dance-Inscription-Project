c55df0750d2b812800af7370f3582b1f890ce19a
OrangeUtang <Joe.dus.sen@gmail.com>
2019-07-04 18:19:55 -0400

    update one
    started the set up options and admin side

---
+++ Languini/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
--- Languini/routes.py
+++ Languini/routes.py
@@ -1,14 +1,22 @@
 import secrets
 import os
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, jsonify, make_response
 from Languini import app, db, bcrypt
-from Languini.models import Movement, Link, AnswersSet, Strain
+from Languini.forms import LoginForm
+from Languini.models import Movement, Answer, Link, Strain, Admin, MoveList
+from flask_login import login_user, current_user, logout_user, login_required
+
+@app.errorhandler(404)
+def page_not_found(e):
+    code = 404
+    msg = "404: Not Found"
+    return render_template('Error.html', title=str(code), msg=msg)
 
 
 @app.route("/")
-@app.route("/home")
+@app.route("/Home")
 def home():
-    return render_template('Home.html')
+    return render_template('Home.html', title="Home")
 
 
 @app.route("/Study")
@@ -17,7 +25,7 @@ def study():
     print()
 
 
-@app.route("/TestForm")
+@app.route("/Test")
 def test_form():
     #TODO
     print()
@@ -27,3 +35,39 @@ def submit():
     #TODO
     print()
 
+
+@app.route("/Admin", methods={"GET"})
+def display_loggin():
+    form = LoginForm()
+    return render_template('Login.html', title="Login", form=form)
+
+
+@app.route("/Admin", methods={"POST"})
+def login():
+    form = LoginForm()
+    # check if credential are valid
+    if form.validate_on_submit():
+        user = Admin.quety.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/Admin/AddVid", methods={"POST"})
+def uploadVidList():
+    """
+    upload a list of vid url and associated ref number to the database
+    take in a string (or file) containing a list of
+    """
+    vid_list = request.form.get("vidList")
+
+
+#Function to set initial User up
+def Factory_setup():
+    """
+    Set admin to defaut values
+    reset database to default values
+    """
+    print()
